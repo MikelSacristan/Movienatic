b568b4fb6cb50eee3579a8d7fcc014ffff4751d2
Bolahh <yangboyiyang@hotmail.com>
2026-09-16 10:52:05 +0200

    first commit

---
+++ src/main/java/com/EnSe/Movienatic/MovienaticApplication.java
@@ -0,0 +1,13 @@
+package com.EnSe.Movienatic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovienaticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovienaticApplication.class, args);
+	}
+
+}
