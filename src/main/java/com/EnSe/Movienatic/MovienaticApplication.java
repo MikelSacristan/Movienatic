cf69963f4aa957585197a8cd9e63b52e03a61992
MikelSC <mikel.sacristan@rai.usc.es>
2026-09-23 09:26:38 +0200

    Cambio a Gradle

--- src/main/java/com/EnSe/Movienatic/MovienaticApplication.java
+++ src/main/java/com/EnSe/Movienatic/MovienaticApplication.java
@@ -1,4 +1,4 @@
-package com.EnSe.Movienatic;
+package com.Ense.Movienatic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
